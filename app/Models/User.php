86e80333c93170c5400a53dda9f1f1777f739c8e
RidhaAdiPurnomo <ridha.adi@mhs.itenas.ac.id>
2025-06-11 15:37:08 +0700

    Setup Spatie Permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements JWTSubject
 {
+    use HasRoles;
+
     // ...
-    
+
     public function getJWTIdentifier()
     {
         return $this->getKey();
