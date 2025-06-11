0c3eb71aa61422176aab1248e2911f2845930990
RidhaAdiPurnomo <ridha.adi@mhs.itenas.ac.id>
2025-06-11 15:47:33 +0700

    Membuat Model dan Relationship

---
+++ app/Models/Project.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+// app/Models/Project.php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'start_date', 'end_date', 'manager_id'];
+
+    public function manager()
+    {
+        return $this->belongsTo(User::class, 'manager_id');
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function members()
+    {
+        return $this->belongsToMany(User::class, 'project_members');
+    }
+}
---
+++ app/Models/Task.php
@@ -0,0 +1,34 @@
+<?php
+
+// app/Models/Task.php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'status', 'due_date', 'project_id', 'assigned_to', 'created_by'];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function assignedTo()
+    {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
